bdd412d9562d3de7cde98c2d9825335f87c2caf9
CarlosSalega <carlosalfredosalega@gmail.com>
2023-06-28 23:06:39 -0200

    6th commit: completed form

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,79 @@
 import {
   Button,
+  Center,
+  Circle,
+  Checkbox,
   Flex,
-  Heading,
+  FormControl,
+  IconButton,
   Input,
+  Link,
+  Text,
   useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
+import {
+  BsFillMoonFill,
+  BsFillSunFill,
+  BsFillLightningChargeFill,
+} from "react-icons/bs";
 
-function App(): React.JSX.Element {
+function App(): JSX.Element {
   const { toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue("gray.100", "gray.700");
 
   return (
-    <Flex h="100vh" alignItems="center" justifyContent="center">
+    <Flex
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <IconButton
+        icon={
+          formBackground === "gray.100" ? <BsFillMoonFill /> : <BsFillSunFill />
+        }
+        aria-label="toggle theme"
+        mb={9}
+        onClick={toggleColorMode}
+      />
       <Flex direction="column" p={12} rounded={6} background={formBackground}>
-        <Heading mb={6} textAlign="center">
-          Log In
-        </Heading>
-        <Input
-          placeholder="example@gmail.com"
-          variant="filled"
-          type="email"
-          mb={6}
-        />
-        <Input
-          placeholder="*********"
-          variant="filled"
-          type="password"
-          mb={6}
-        />
-        <Button colorScheme="teal" mb={6}>
+        <Center pb={6}>
+          <Circle bg="teal.400" size="40px" color="white">
+            {<BsFillLightningChargeFill size={24} />}
+          </Circle>
+        </Center>
+
+        <FormControl pb={6}>
+          <Input type="email" placeholder="example@mail.com" variant="filled" />
+        </FormControl>
+        <FormControl pb={2}>
+          <Input
+            type="password"
+            placeholder="***************"
+            variant="filled"
+          />
+        </FormControl>
+        <Link mb={6} fontSize="xs" color="teal.400">
+          Forgot your password?
+        </Link>
+        <Button
+          bg="teal.400"
+          mb={2}
+          color="white"
+          _hover={{ bg: "teal.200", color: "black" }}
+        >
           Log In
         </Button>
-        <Button onClick={toggleColorMode}>Toogle Color Mode</Button>
+        <Checkbox colorScheme="teal" mb={6}>
+          <Text fontSize="xs">Remember me</Text>
+        </Checkbox>
+        <Text textAlign="center" mb={-4} fontSize="xs">
+          or{" "}
+          <Link fontSize="sm" color="teal.400">
+            Sign Up
+          </Link>
+        </Text>
       </Flex>
     </Flex>
   );
